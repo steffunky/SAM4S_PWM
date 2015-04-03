ab1fbf30570bd5fdf13df4f46173f20a06c41038
rob1fo <alonzo.robin7@gmail.com>
2015-04-03 14:05:08 +0200

    Codage du mock

---
+++ pwm_mock.c
@@ -0,0 +1,66 @@
+#include <pwm_mock.h>
+
+CODE int set(FAR struct pwm_lowerhalf_s *dev)
+{
+	puts("1");
+	return 0;
+}
+
+CODE int shut(FAR struct pwm_lowerhalf_s *dev)
+{
+	puts("2");
+	return 0;
+}
+
+CODE int start(FAR struct pwm_lowerhalf_s *dev,
+			   FAR const struct pwm_info_s *info)
+{
+	puts("3");
+	return 0;	
+}
+
+CODE int stop(FAR struct pwm_lowerhalf_s *dev)
+{
+	puts("4");
+	return 0;
+}
+
+FAR const struct pwm_ops_s sam4s_pwm_vt =
+{
+	&set,
+	&shut,
+	&start,
+	&stop,
+	&ioctl
+};
+
+struct pwm_lowerhalf_s sam4s_pwm =
+{
+	&sam4s_pwm_vt
+};
+
+
+void parse_args(struct pwm_state_s* g_pwmstate,int argc , char *argv[] ){
+	puts("parse_args");
+}
+
+int ioctle(int fd, int command, void * arg){
+	puts("ioctl");
+	return 2;
+};
+
+int opene(char* path, int param){
+	puts("open");
+	return 3;
+}
+
+int pwm_register(FAR const char *path, FAR struct pwm_lowerhalf_s *dev){
+	puts("pwm_register");
+	return 4;
+}
+
+int pwm_devinit(){
+	puts("pwm_devinit");
+	pwm_register("/dev/pwm0", &sam4s_pwm);
+	return 8;
+}
--- sam4pwm.c
+++ sam4pwm.c
@@ -1,12 +1,55 @@
-#include <nuttx/config.h>
-#include <stdio.h>
 
-#ifdef CONFIG_BUILD_KERNEL
-int main(int argc, FAR char *argv[])
-#else
-int sam4pwm_main(int argc, char *argv[])
-#endif
+//#include <nuttx/config.h>
+#include <stdio.h>
+#include <stdint.h>
+//#include <nuttx/pwm.h>
+
+#include "pwm_mock.h"
+
+static struct pwm_state_s g_pwmstate;
+
+static void pwm_devpath(struct pwm_state_s *pwm, const char *devpath)
 {
-  printf("Hello, World!!\n");
-  return 0;
+    if (pwm->devpath)
+    {
+      free(pwm->devpath);
+    }
+
+  pwm->devpath = strdup(devpath);
+}
+
+int main(int argc, char *argv[])
+{
+    printf("Hello, World!!\n");
+
+    struct pwm_info_s info;
+    int fd;
+    int ret;
+
+    ret = pwm_devinit();
+
+    if (!g_pwmstate.initialized)
+    {
+        g_pwmstate.duty        = 50;
+        g_pwmstate.freq        = 100;
+        g_pwmstate.duration    = 5;
+        g_pwmstate.initialized = true;
+    }
+
+    parse_args(&g_pwmstate, argc, argv);
+
+    if (!g_pwmstate.devpath){
+        pwm_devpath(&g_pwmstate, "/dev/pwm0");
+    }
+
+    fd = opene(g_pwmstate.devpath, O_RDONLY);
+
+    info.frequency = g_pwmstate.freq;
+    info.duty      = ((uint32_t)g_pwmstate.duty << 16) / 100;
+
+    ret = ioctle(fd, PWMIOC_SETCHARACTERISTICS, (unsigned long)((uintptr_t)&info));
+
+    ret = ioctle(fd, PWMIOC_START, 0);
+
+    return 0;
 }
